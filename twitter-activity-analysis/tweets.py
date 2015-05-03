d650c73a182d8fd4f1689c97ec9c5518eb409e51
Ben Hilburn <bhilburn@gmail.com>
2015-05-03 11:46:08 -0700

    successfully pulling down entire timeline sans replies

--- twitter-activity-analysis/tweets.py
+++ twitter-activity-analysis/tweets.py
@@ -1,6 +1,5 @@
 import requests
 import base64
-import json
 
 # Let's get our bearer token
 auth_file = open('auth.txt', 'r')
@@ -22,6 +21,8 @@ token_json = token_response.json()
 token = token_json["access_token"]
 
 # Cool. We are authenticated. Now let's pull down the timeline.
+tweets = list()
+
 timeline_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
 query_headers = {'Authorization': 'Bearer {}'.format(token),
                  'User-Agent': 'Content Analyzer'}
@@ -29,7 +30,21 @@ query_params = {'screen_name': 'EttusResearch',
                 'exclude_replies': 'true',
                 'count': '200'}
 
-timeline_response = requests.get(timeline_url, headers=query_headers, params=query_params)
-timeline_json = timeline_response.json()
+#print json.dumps(timeline_json, sort_keys=True, indent=4, separators=(',', ': '))
 
-print json.dumps(timeline_json, sort_keys=True, indent=4, separators=(',', ': '))
+last_id = -1
+
+while True:
+    timeline_response = requests.get(timeline_url, headers=query_headers, params=query_params)
+    timeline_json = timeline_response.json()
+
+    max_id = timeline_json[len(timeline_json) - 1]["id"]
+
+    if(max_id == last_id):
+        break
+    else:
+        last_id = max_id
+        tweets.extend(timeline_json)
+        query_params['max_id'] = '{}'.format(max_id)
+
+print "Pulled {} tweets.".format(len(tweets))
