4461937480d7d52aee4ecc00d4472d96cdeb9e9f
Ben Hilburn <bhilburn@gmail.com>
2015-05-02 22:55:22 -0700

    successfully pulling down timelines

--- twitter-activity-analysis/tweets.py
+++ twitter-activity-analysis/tweets.py
@@ -1,7 +1,6 @@
-import os
-import glob
 import requests
 import base64
+import json
 
 # Let's get our bearer token
 auth_file = open('auth.txt', 'r')
@@ -17,6 +16,20 @@ auth_header = {'Authorization': 'Basic {}'.format(base64_token_creds),
                'User-Agent': 'Content Analyzer'}
 auth_data = {'grant_type': 'client_credentials'}
 
-token = requests.post(auth_url, headers=auth_header, data=auth_data)
+token_response = requests.post(auth_url, headers=auth_header, data=auth_data)
+token_json = token_response.json()
 
-print token.text
+token = token_json["access_token"]
+
+# Cool. We are authenticated. Now let's pull down the timeline.
+timeline_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+query_headers = {'Authorization': 'Bearer {}'.format(token),
+                 'User-Agent': 'Content Analyzer'}
+query_params = {'screen_name': 'EttusResearch',
+                'exclude_replies': 'true',
+                'count': '200'}
+
+timeline_response = requests.get(timeline_url, headers=query_headers, params=query_params)
+timeline_json = timeline_response.json()
+
+print json.dumps(timeline_json, sort_keys=True, indent=4, separators=(',', ': '))
